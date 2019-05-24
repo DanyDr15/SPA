3c8ca2507430753b4220fd40e433d9cc1d90bbb5
DanyDr15 <40213455+DanyDr15@users.noreply.github.com>
2019-05-24 11:46:52 -0500

    quitando cositas

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "dapianchiche@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
